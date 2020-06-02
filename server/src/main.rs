8ca2f7c31e078a4c8f10d8c355559eb52cd6c34d
dongfeng <18510416169@qq.com>
2020-06-02 09:25:05 +0800

    add udp-client udp-server

---
+++ server/src/main.rs
@@ -0,0 +1,11 @@
+use std::net::UdpSocket;
+
+fn main() {
+    let socket = UdpSocket::bind("0.0.0.0:8888").unwrap();
+    let mut buf = [0u8; 65535];
+    loop {
+        let (amt, src) = socket.recv_from(&mut buf).unwrap();
+        println!("received {} bytes from: {:?}", amt, src);
+        println!("{:?}", &buf[0..10]);
+    }
+}
