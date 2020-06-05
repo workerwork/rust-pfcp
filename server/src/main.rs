d41821edcc30f7572b2364b884c56fa84e303460
dongfeng <18510416169@qq.com>
2020-06-05 16:55:45 +0800

    add config

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,27 @@
 use messages::Message;
 use std::net::UdpSocket;
 
+extern crate yaml_rust;
+
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use yaml_rust::yaml;
+
 fn main() {
-    let socket = UdpSocket::bind("0.0.0.0:8888").unwrap();
+    let args: Vec<_> = env::args().collect();
+    let mut f = File::open(&args[1]).unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).unwrap();
+
+    let docs = yaml::YamlLoader::load_from_str(&s).unwrap();
+
+    let ip = &docs[0]["servers"][0]["server"]["ip"].as_str().unwrap();
+    let port = &docs[0]["servers"][0]["server"]["port"].as_str().unwrap();
+
+    let addr = format!("{}:{}", ip, port);
+
+    let socket = UdpSocket::bind(addr).unwrap();
     let mut buf = [0u8; 65535];
     loop {
         let (amt, src) = socket.recv_from(&mut buf).unwrap();
