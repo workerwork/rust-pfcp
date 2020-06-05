c7e54b893ac9fc15d40797b5a43502e011e1789a
dongfeng <18510416169@qq.com>
2020-06-05 14:35:37 +0800

    update

--- messages/src/association_setup_request.rs
+++ messages/src/association_setup_request.rs
@@ -13,29 +13,26 @@ pub struct AssociationSetupRequest {
 
 impl AssociationSetupRequest {
     pub fn parse(mut buf: &mut [u8], header: Header) -> Message {
-        let mut associationsetuprequest = AssociationSetupRequest {
+        let mut message = AssociationSetupRequest {
             header,
             ..Default::default()
         };
-        if associationsetuprequest.header.s == true {
+        if message.header.s == true {
             buf = &mut buf[12..];
         } else {
             buf = &mut buf[4..];
         }
         //let cursor: u16 = 0;
-        //while cursor < associationsetuprequest.header.msg_len {
         while buf != [] {
             let etype: u16 = (buf[0] * 16 + buf[1]).into();
             let elen: u16 = (buf[2] * 16 + buf[3]).into();
             buf = &mut buf[4..];
             match etype {
-                //t if t == IEType::NodeID as u16 => {
                 ie_type::NODE_ID => {
-                    associationsetuprequest.node_id = NodeID::decode(buf, elen);
+                    message.node_id = NodeID::decode(buf, elen);
                 }
-                //t if t == IEType::RecoveryTimeStamp as u16 => {
                 ie_type::RECOVERY_TIME_STAMP => {
-                    associationsetuprequest.recovery_time_stamp =
+                    message.recovery_time_stamp =
                         RecoveryTimeStamp::decode(buf, elen);
                 }
                 _ => println!(""),
@@ -43,8 +40,8 @@ impl AssociationSetupRequest {
             buf = &mut buf[elen.into()..];
             //let cursor = cursor + elen + 4;
         }
-        println!("{:#?}", associationsetuprequest);
-        Message::ASR(associationsetuprequest)
+        println!("{:#?}", message);
+        Message::ASR(message)
     }
 
     pub fn pack(self) -> Vec<u8> {
