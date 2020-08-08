f51a022a311f0c27132c122a5abfe95bab00cbcc
wow <workerwork@qq.com>
2020-08-09 01:38:04 +0800

    update

--- messages/src/lib.rs
+++ messages/src/lib.rs
@@ -3,7 +3,7 @@ pub mod association_setup_request;
 pub mod association_update_request;
 pub mod elements;
 pub mod header;
-pub mod msg_type;
+//pub mod msg_type;
 pub mod session_deletion_request;
 pub mod session_establishment_request;
 pub mod session_modification_request;
@@ -16,6 +16,33 @@ use header::*;
 use session_deletion_request::*;
 use session_establishment_request::*;
 use session_modification_request::*;
+
+//define message type
+pub mod msg_type {
+    pub const HEARTBEAT_REQUEST: u8 = 1;
+    pub const HEARTBEAT_RESPONSE: u8 = 2;
+    pub const PFD_MANAGEMENT_REQUEST: u8 = 3;
+    pub const PFD_MANAGEMENT_RESPONSE: u8 = 4;
+    pub const ASSOCIATION_SETUP_REQUEST: u8 = 5;
+    pub const ASSOCIATION_SETUP_RESPONSE: u8 = 6;
+    pub const ASSOCIATION_UPDATE_REQUEST: u8 = 7;
+    pub const ASSOCIATION_UPDATE_RESPONSE: u8 = 8;
+    pub const ASSOCIATION_RELEASE_REQUEST: u8 = 9;
+    pub const ASSOCIATION_RELEASE_RESPONSE: u8 = 10;
+    pub const VERSION_NOT_SUPPORTED: u8 = 11;
+    pub const NODE_REPORT_REQUEST: u8 = 12;
+    pub const NODE_REPORT_RESPONSE: u8 = 13;
+    pub const SESSION_SET_DELETION_REQUEST: u8 = 14;
+    pub const SESSION_SET_DELETION_RESPONSE: u8 = 15;
+    pub const SESSION_ESTABLISHMENT_REQUEST: u8 = 50;
+    pub const SESSION_ESTABLISHMENT_RESPONSE: u8 = 51;
+    pub const SESSION_MODIFICATION_REQUEST: u8 = 52;
+    pub const SESSION_MODIFICATION_RESPONSE: u8 = 53;
+    pub const SESSION_DELETION_REQUEST: u8 = 54;
+    pub const SESSION_DELETION_RESPONSE: u8 = 55;
+    pub const SESSION_REPORT_REQUEST: u8 = 56;
+    pub const SESSION_REPORT_RESPONSE: u8 = 57;
+}
 
 //define error
 use std::io;
