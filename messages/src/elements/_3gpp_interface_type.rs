7d8ced12bb0e45c4e75ae86b793a5e7266df1252
wow <workerwork@qq.com>
2020-08-10 21:44:34 +0800

    update

--- messages/src/elements/_3gpp_interface_type.rs
+++ messages/src/elements/_3gpp_interface_type.rs
@@ -2,29 +2,29 @@ use super::super::PFCPError;
 use super::ie_type;
 
 // -----------------------------------------------------------------------
-//	                    Bits
+// 	                    Bits
 // Octets	8	7	6	5	4	3	2	1
-// 1 to 2	    Type = 42 (decimal)
+// 1 to 2	    Type = 160 (decimal)
 // 3 to 4	    Length = n
-// 5 	        Spare	Interface value
+// 5 	        Spare	Interface Type value
 // 6 to (n+4)	These octet(s) is/are present only if explicitly specified
 // -----------------------------------------------------------------------
 
 #[derive(Debug, Default)]
-pub struct DestinationInterface {
+pub struct _3GPPInterfaceType {
     ie_type: u16,
     ie_len: u16,
-    interface: u8, //M
+    interface_type: u8, //M
 }
 
-impl DestinationInterface {
-    pub fn decode(buf: &[u8], len: u16) -> Result<DestinationInterface, PFCPError> {
-        let mut element = DestinationInterface {
-            ie_type: ie_type::DESTINATION_INTERFACE,
+impl _3GPPInterfaceType {
+    pub fn decode(buf: &[u8], len: u16) -> Result<_3GPPInterfaceType, PFCPError> {
+        let mut element = _3GPPInterfaceType {
+            ie_type: ie_type::_3GPP_INTERFACE_TYPE,
             ie_len: len,
             ..Default::default()
         };
-        element.interface = buf[0] & 0b0000_1111;
+        element.interface_type = buf[0] & 0b0011_1111;
         Ok(element)
     }
 
@@ -32,7 +32,7 @@ impl DestinationInterface {
         let mut element_vec: Vec<u8> = Vec::new();
         element_vec.append(&mut self.ie_type.to_be_bytes().to_vec());
         element_vec.append(&mut self.ie_len.to_be_bytes().to_vec());
-        element_vec.push(self.interface);
+        element_vec.push(self.interface_type);
         element_vec
     }
 }
--- messages/src/elements/apply_action.rs
+++ messages/src/elements/apply_action.rs
@@ -1,1 +1,77 @@
+use super::super::PFCPError;
+use super::ie_type;
+
+// -----------------------------------------------------------------------
+//	                    Bits
+// Octets	8	7	6	5	4	3	2	1
+// 1 to 2	    Type = 44 (decimal)
+// 3 to 4	    Length = n
+// 5	DFRT	IPMD	IPMA	DUPL	NOCP	BUFF	FORW	DROP
+// 6	        Spare	DDPN	BDPN	EDRT
+// 7 to (n+4)	These octet(s) is/are present only if explicitly specified
+// -----------------------------------------------------------------------
+
+#[derive(Debug, Default)]
+pub struct ApplyAction {
+    ie_type: u16,
+    ie_len: u16,
+    
+    //Bit 1 – DROP (Drop): when set to "1", this indicates a request to drop the packets.
+    //
+    //Bit 2 – FORW (Forward): when set to "1", this indicates a request to forward the packets.
+    //
+    //Bit 3 – BUFF (Buffer): when set to "1", this indicates a request to buffer the packets.
+    //
+    //Bit 4 – NOCP (Notify the CP function): when set to "1", this indicates a request to notify
+    //the CP function about the arrival of a first downlink packet being buffered.
+    //
+    //Bit 5 – DUPL (Duplicate): when set to "1", this indicates a request to duplicate the packets.
+    //
+    //Bit 6 – IPMA (IP Multicast Accept): when set to "1", this indicates a request to accept UE
+    //requests to join an IP multicast group.
+    //
+    //Bit 7 – IPMD (IP Multicast Deny): when set to "1", this indicates a request to deny UE
+    //requests to join an IP multicast group.
+    //
+    //Bit 8 – DFRT (Duplicate for Redundant Transmission): when set to "1", this indicates a
+    //request to duplicate the packets for redundant transmission.
+    action: u8,   //M
+
+    //Bit 1 – EDRT (Eliminate Duplicate Packets for Redundant Transmission): when set to "1", this
+    //indicates a request to eliminate duplicate packets used for redundant transmission.
+    //
+    //Bit 2 – BDPN (Buffered Downlink Packet Notification): when set to "1", this indicates a
+    //request to notify the CP function about the first buffered DL packet for downlink data
+    //delivery status notification.
+    //
+    //Bit 3 – DDPN (Discarded Downlink Packet Notification): when set to "1", this indicates a
+    //request to notify the CP function about the first discarded DL packet for downlink data
+    //delivery status notification if the DL Buffering Duration or DL Buffering Suggested Packet
+    //Count is exceeded.
+    //
+    //Bit 4 to 8 – Spare, for future use and seto to "0".
+    mask: u8,   //M
+}
+
+impl ApplyAction {
+    pub fn decode(buf: &[u8], len: u16) -> Result<ApplyAction, PFCPError> {
+        let mut element = ApplyAction {
+            ie_type: ie_type::APPLY_ACTION,
+            ie_len: len,
+            ..Default::default()
+        };
+        element.action = buf[0];
+        element.mask = buf[1] & 0b0000_0111;
+        Ok(element)
+    }
+
+    pub fn encode(mut self) -> Vec<u8> {
+        let mut element_vec: Vec<u8> = Vec::new();
+        element_vec.append(&mut self.ie_type.to_be_bytes().to_vec());
+        element_vec.append(&mut self.ie_len.to_be_bytes().to_vec());
+        element_vec.push(self.action);
+        element_vec.push(self.mask);
+        element_vec
+    }
+}
 
